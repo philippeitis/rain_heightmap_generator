95171c1b322cfbbc4604b75f174705f30b4be153
l8f2v <l8f2b@ugrad.cs.ubc.ca>
2019-06-08 13:42:29 -0700

    Added support for hemispheres, but they currently render as squares, so by default drops are treated as mono-hemispherical. Additionally, added support for multiple runs and bordered images via command line flags (--runs, --border)

--- sim.py
+++ sim.py
@@ -1,50 +1,73 @@
-## Constants per the paper
-import PIL
 import math
 import scipy.stats as st
 import numpy as np
 import random
 from scipy import ndimage
 
+
 class Droplet:
-    def __init__(self, x, y, mass, velocity):
+    def __init__(self, x, y, mass, velocity=0):
         self.x = x
         self.y = y
         self.mass = mass
         self.velocity = velocity
         self.direction = 0
         self.t_i = 0
-        self.hemispheres = [] # (x,y) tuples (could add z to represent share of mass)
+        self.hemispheres = [(self.x, self.y)]  # (x,y) tuples (could add z to represent share of mass)
         if mass < m_static and hemispheres_enabled:
-             self.generate_hemispheres()
+            self.generate_hemispheres()
 
     def generate_hemispheres(self):
-        num_hemispheres = random.randint(1,max_hemispheres)
+        self.hemispheres = [(self.x,self.y)]
+        num_hemispheres = random.randint(1, max_hemispheres)
+        new_x = self.x
+        new_y = self.y
+        while len(self.hemispheres) < num_hemispheres:
+            direction = random.randint(1, 4)
+            if direction == 1:  # Down
+                new_y -= 1
+            if direction == 2:  # Left
+                new_x -= 1
+            if direction == 3:  # Up
+                new_y += 1
+            if direction == 4:  # Right
+                new_x += 1
+
+            if not (new_x, new_y) in self.hemispheres:
+                self.hemispheres.append((new_x,new_y))
 
     def iterate_position(self):
-        acceleration = (self.mass * gravity - friction_constant_force)/self.mass
-        self.velocity = self.velocity + acceleration * time_step
-        if self.velocity > 0:
-            self.direction = choose_direction(self)
-            if self.direction == -1:
-                self.x -= math.floor(math.sqrt(self.velocity) / scale_factor * width)
-                self.y += math.floor(math.sqrt(self.velocity) / scale_factor * width)
-            if self.direction == 0:
-                self.y += math.floor(self.velocity * scale_factor * width)
-            if self.direction == 1:
-                self.x += math.floor(math.sqrt(self.velocity) / scale_factor * width)
-                self.y += math.floor(math.sqrt(self.velocity) / scale_factor * width)
+        if self.mass > m_static:
+            acceleration = (self.mass * gravity - friction_constant_force)/self.mass
+            self.velocity = self.velocity + acceleration * time_step
+            if self.velocity > 0:
+                self.direction = choose_direction(self)
+                if self.direction == -1:
+                    self.x -= math.floor(math.sqrt(self.velocity) * scale_factor * width)
+                    self.y += math.floor(math.sqrt(self.velocity) * scale_factor * width)
+                if self.direction == 0:
+                    self.y += math.floor(self.velocity * scale_factor * width)
+                if self.direction == 1:
+                    self.x += math.floor(math.sqrt(self.velocity) * scale_factor * width)
+                    self.y += math.floor(math.sqrt(self.velocity) * scale_factor * width)
 
-            # This determines if we should leave a droplet behind
-            self.t_i += 1
+                # This determines if we should leave a droplet behind
+                self.t_i += 1
 
     def intersects(self, drop):
-        delta_x = self.x - drop.x
-        delta_y = self.y - drop.y
-        return math.sqrt(delta_x**2 + delta_y**2) < drop.radius() + self.radius()
+        if drop.get_lowest_y() < self.get_highest_y() or drop.get_highest_y() > self.get_lowest_y():
+            return False
+
+        for self_x, self_y in self.hemispheres:
+            for drop_x, drop_y in drop.hemispheres:
+                delta_x = self_x - drop_x
+                delta_y = self_y - drop_y
+                if math.sqrt(delta_x**2 + delta_y**2) < drop.radius() + self.radius():
+                    return True
+        return False
 
     def radius(self):
-        return np.cbrt((3 / 2) / math.pi * self.mass / density_water) / scale_factor * width
+        return np.cbrt((3 / 2) / math.pi * (self.mass / len(self.hemispheres)) / density_water) / scale_factor * width
 
     def residual_probability(self):
         if self.velocity > 0:
@@ -52,8 +75,50 @@ class Droplet:
         else:
             return 0
 
+    def get_lowest_y(self):
+        lowest_y = height
+        for x, y in self.hemispheres:
+            if y < lowest_y:
+                lowest_y = y
+        return lowest_y - math.floor(self.radius())
+
+    def get_highest_y(self):
+        highest_y = 0
+        for x, y in self.hemispheres:
+            if y > highest_y:
+                highest_y = y
+        return highest_y + math.floor(self.radius())
+
+    def get_lowest_x(self):
+        lowest_x = width
+        for x, y in self.hemispheres:
+            if x < lowest_x:
+                lowest_x = x
+        return lowest_x - math.floor(self.radius())
+
+    def get_highest_x(self):
+        highest_x = 0
+        for x, y in self.hemispheres:
+            if x > highest_x:
+                highest_x = x
+        return highest_x + math.floor(self.radius())
+
+    def get_height(self, x, y):
+        if len(self.hemispheres) >= 1:
+            return np.sqrt(self.radius() ** 2 - (y - self.y) ** 2 - (x - self.x) ** 2)
+        else:
+            summation = 0
+            for x, y in self.hemispheres:
+                local_height = np.sqrt(self.radius() ** 2 - (y - self.y) ** 2 - (x - self.x) ** 2)
+                if local_height > 0:
+                    summation += local_height
+            return summation
+
 
 def choose_direction(droplet):
+    # Chooses three regions in front of the drop, calculates total water volume
+    # and returns region with highest water volume, or random if equal quantities of
+    # water present
     radius = math.floor(droplet.radius())
     start_y = droplet.y + radius
     end_y = start_y + radius * 2
@@ -96,7 +161,7 @@ def choose_direction(droplet):
 def add_drops(avg):
     for x in range(avg):
         mass = min(m_max,max(m_min,np.random.normal(average_mass, deviation_mass, 1)))
-        drop_to_add = Droplet(random.randint(0, width), random.randint(0, height), mass, 0)
+        drop_to_add = Droplet(random.randint(0, width), random.randint(0, height), mass)
         drop_array.append(drop_to_add)
 
 
@@ -126,20 +191,15 @@ def compute_height_map():
 
 def update_height_map():
     for drop in drop_array:
-        radius = drop.radius()
-        round_radius = math.floor(radius)
-        for y in range(drop.y - round_radius, drop.y + round_radius+1):
-            for x in range(drop.x - round_radius, drop.x + round_radius+1):
+        for y in range(drop.get_lowest_y(), drop.get_highest_y() + 1):
+            for x in range(drop.get_lowest_x(), drop.get_highest_x() + 1):
                 if (0 <= y < height) and (0 <= x < width):
-                    new_height = np.sqrt(radius**2 - (y - drop.y)**2 - (x-drop.x)**2)
+                    new_height = drop.get_height(x, y)
                     if height_map[x][y] < new_height:
                         height_map[x][y] = new_height
 
 
 def smooth_height_map():
-    kernel = np.array([[1/9, 1/9, 1/9],
-                       [1/9, 1/9, 1/9],
-                       [1/9, 1/9, 1/9]])
     global height_map
     height_map = ndimage.convolve(height_map, kernel, mode='constant', cval=0)
 
@@ -152,7 +212,7 @@ def detect_intersections():
     detections = []
 
     for a in range(len(drop_array)):
-        for b in range(a+1,len(drop_array)):
+        for b in range(a+1, len(drop_array)):
             if drop_array[a].intersects(drop_array[b]):
                 detections.append((drop_array[a], drop_array[b]))
 
@@ -170,11 +230,15 @@ def merge_drops():
             if a.y < b.y:
                 a.velocity = new_velocity
                 a.mass += b.mass
+                if a.mass < m_static and hemispheres_enabled:
+                    a.generate_hemispheres()
                 drop_array.remove(b)
 
             else:
                 b.velocity = new_velocity
                 b.mass += a.mass
+                if b.mass < m_static and hemispheres_enabled:
+                    b.generate_hemispheres()
                 drop_array.remove(a)
 
 
@@ -198,8 +262,17 @@ def generate_time_stamp():
 
 
 def save(filename, fformat):
+    import PIL
+    border = int(args.border)
+
+    height_map[0:border] = 0
+    height_map[height - border:] = 0
+    height_map[:, 0:border] = 0
+    height_map[:, width - border:] = 0
+
     if fformat == "txt":
         np.savetxt(filename + ".txt", height_map, delimiter=",")
+
     elif fformat == "png":
         from PIL import Image
         maximum_drop_size = np.amax(height_map)
@@ -226,6 +299,13 @@ def set_up_directories():
             print("Directory already exists.")
 
 
+def padded_zeros(ref_string, curr_num):
+    out_string = str(curr_num)
+    while len(out_string) < len(ref_string):
+        out_string = "0" + out_string
+    return out_string
+
+
 if __name__ == '__main__':
     import argparse
 
@@ -244,8 +324,12 @@ if __name__ == '__main__':
 
     parser.add_argument('--drops', dest='drops', default=5,
                         help='average number of drops per time step')
+
     parser.add_argument('--residual_drops', dest='leave_residuals', default=False,
                         help='enable residual drops')
+    parser.add_argument('--kernel', dest='kernel', default="dwn",
+                        help='Type of kernel used in smoothing step. '
+                             '(dwn for downward trending, avg for averaging kernel)')
 
     parser.add_argument('--mmin', dest='m_min', default=0.000001,
                         help='minimum mass of droplets (kg)')
@@ -257,10 +341,12 @@ if __name__ == '__main__':
                         help='maximum mass of droplets (kg)')
     parser.add_argument('--mstatic', dest='m_static', default=0.8,
                         help='percentage of drops that do not move ')
+
     parser.add_argument('--hemispheres', dest='enable_hemispheres', default=True,
                         help='enable hemisphere based drops')
     parser.add_argument('--numh', dest='max_hemispheres', default=5,
                         help='maximum number of hemispheres per drop')
+
     parser.add_argument('--g', dest='g', default=9.81,
                         help='gravitational constant (m/s)')
 
@@ -278,6 +364,11 @@ if __name__ == '__main__':
 
     parser.add_argument('--f', dest='format', default="png",
                         help='file format (txt file with comma seperated rows), or png.')
+    parser.add_argument('--border', dest='border', default=0,
+                        help='all values within border pixels of the edge will be 0')
+
+    parser.add_argument('--runs', dest='runs', default=1,
+                        help='number of images to create.')
 
     args = parser.parse_args()
 
@@ -301,28 +392,58 @@ if __name__ == '__main__':
     time_step = args.time
     t_max = time_step * 4
 
-    drop_array = []
-    height_map = np.zeros(shape=(width, height))
-
     temp_name = "temp"
     temp_path = "./temp/"
 
     set_up_directories()
 
-    for i in range(int(args.steps)):
-        add_drops(int(args.drops))
-        iterate_over_drops()
-        if bool(args.leave_residuals):
-            leave_residual_droplets()
-        update_height_map()
-        compute_height_map()
-        merge_drops()
-        trim_drops()
-        print("\rStep " + str(i+1) + " out of " + args.steps + " is complete.")
+    kernel = np.array(1)
 
-    if args.format != "none":
+    if args.kernel == "dwn":
+        kernel = np.array([[4/27, 1/9, 2/27],
+                       [4/27, 1/9, 2/27],
+                       [4/27, 1/9, 2/27]])
+    else:
+        kernel = np.array([[1/9, 1/9, 1/9],
+                        [1/9, 1/9, 1/9],
+                        [1/9, 1/9, 1/9]])
 
-        if args.name:
-            save(args.path+args.name, args.format)
+    name = ""
+    if args.name:
+        name = args.name
+    else:
+        name = generate_time_stamp()
+
+    for i in range(int(args.runs)):
+
+        file_name = ""
+        drop_array = []
+        height_map = np.zeros(shape=(width, height))
+
+        if int(args.runs) > 1:
+            if not args.name:
+                file_name = name + "_" + padded_zeros(args.runs, i)
+            else:
+                file_name = name + padded_zeros(args.runs, i)
         else:
-            save(args.path + generate_time_stamp(), args.format)
+            file_name = name
+
+        for ii in range(int(args.steps)):
+            add_drops(int(args.drops))
+            iterate_over_drops()
+            if bool(args.leave_residuals):
+                leave_residual_droplets()
+            update_height_map()
+            compute_height_map()
+            merge_drops()
+            trim_drops()
+            print("\rStep " + str(ii+1) + " out of " + args.steps + " is complete.")
+
+        if args.format != "none":
+            if args.name:
+                save(args.path + file_name, args.format)
+            else:
+                save(args.path + file_name, args.format)
+
+        if (int(args.runs)) > 1:
+            print("\rRun " + str(i + 1) + " out of " + args.runs + " is complete.")
